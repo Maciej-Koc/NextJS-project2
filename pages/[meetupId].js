0fc9cb45221debe190bbcc72a28d3da98b885d2f
Maciej-Koc <mkoc2@asu.edu>
2022-08-06 00:26:33 -0500

    Included wrapper for layout. Included custom component for meetup detail with CSS. Additionally, began working on HomePage with using useEffect and prerendering the data.

---
+++ pages/[meetupId].js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Bryggen%2C_Bergen3.JPG/1280px-Bryggen%2C_Bergen3.JPG"
+      address="Some Street 5, 555"
+      description="E Plurbus Unum Go"
+    />
+  );
+}
